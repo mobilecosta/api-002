91bbac7719fdc609a15cb5793d974708ffdf626e
Wagner Mobile Costa <mobile.costa@gmail.com>
2026-02-26 17:09:02 -0300

    Realizar commit para deploy

---
+++ .run/start-api-local.js
@@ -0,0 +1,4 @@
+const { app } = require('../dist/app');
+app.listen(3000, () => {
+  console.log('API listening on http://localhost:3000');
+});
---
+++ .run/start-api-local.ts
@@ -0,0 +1,5 @@
+import { app } from "../src/app";
+
+app.listen(3000, () => {
+  console.log("API listening on http://localhost:3000");
+});
